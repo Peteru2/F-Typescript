4fff294a4e6f233ff8665c989e37a156fec2d522
DevPeter <polalekan526@gmail.com>
2023-06-21 15:56:27 +0100

    initial commit

---
+++ Food.js
@@ -0,0 +1,4 @@
+var Yname = "Peter";
+console.log(Yname);
+var yeat = "This is a new year";
+console.log(yeat);
---
+++ Food.ts
@@ -0,0 +1,5 @@
+const Yname = "Peter";
+
+console.log(Yname);
+const yeat = "This is a new year";
+console.log("Things aren't going has planned");
